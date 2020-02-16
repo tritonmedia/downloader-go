ae06183a658b87d0138ff87deba2d26256a26f52
Jared Allard <jaredallard@outlook.com>
2020-02-16 10:20:16 -0800

    feat: add basic processor

---
+++ internal/process/process.go
@@ -0,0 +1,50 @@
+// Package process scans a dir for media files, and some pre-set conditions
+// to determine which media files should have a job created for them to later
+// be evaluated by Twilight
+package process
+
+import (
+	"os"
+	"path/filepath"
+)
+
+// TODO(jaredallard): running files through ffprobe is likely to end up
+// working better than file extensions
+// valid media file extensions
+var mediaExts = map[string]bool{
+	".mp4":  true,
+	".mkv":  true,
+	".mov":  true,
+	".webm": true,
+}
+
+// TODO(jaredallard): add filtering dirs we "enter"
+// ProcessDir finds media files in a dir and returns their paths
+func ProcessDir(path string) ([]string, error) {
+	files := []string{}
+	err := filepath.Walk(path, func(file string, info os.FileInfo, err error) error {
+		// skip directories
+		if info.IsDir() {
+			return nil
+		}
+
+		// walk had an error :( so we die
+		if err != nil {
+			return err
+		}
+
+		ext := filepath.Ext(file)
+		if !mediaExts[ext] {
+			return nil
+		}
+
+		files = append(files, file)
+
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return files, nil
+}
