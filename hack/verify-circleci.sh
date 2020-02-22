898fcf0aac21349b7607c43785e3968622e11641
Jared Allard <jaredallard@outlook.com>
2020-02-22 15:15:49 -0800

    feat(ci): show diff on config.yaml change

--- hack/verify-circleci.sh
+++ hack/verify-circleci.sh
@@ -7,6 +7,7 @@ make render-circle
 
 if git diff-index --name-status HEAD | grep .circleci/config.yml; then
   # we succeeded, so we fail
+  git diff .circleci/config.yml
   echo "Error: .circleci/circle.jsonnet was not rendered, or an update to the template is available"
   echo " Run 'make render-circleci' and commit the result"
   exit 1
